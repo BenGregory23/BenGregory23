efdde89aff8f4e650807e119a12a5a6fc4d9e7b7
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-03 21:54:03 +0200

    modified function

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 
-//getting element
-let text = document.getElementById('about-me');
 
-text.onmousedown = function(event) { //  start the process
-    // get ready to move: make an absolute and top z-index
-    text.style.position = 'absolute';
-    text.style.zIndex = 1000;
-    // move it from any existing parents directly to the body
-    // to position it relative to the body
-    document.body.append(text);
-    // and put this absolutely positioned text under the pointer
-    moveAt(event.clientX, event.clientY);
-    // centers the text on the coordinates (pageX, pageY)
-    function moveAt(pageX, pageY) {
-      text.style.left = pageX - text.offsetWidth / 2 + 'px';
-      text.style.top = pageY - text.offsetHeight / 2 + 'px';
-    }
-    function onMouseMove(event) {
-      moveAt(event.clientX, event.clientY);
-    }
-    //  move the text on mousemove
-    document.addEventListener('mousemove', onMouseMove);
-    // drop the text, remove unneeded handlers
-    text.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      text.onmouseup = null;
-    };
-};
-
-let map
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+// get every element with the class card 
+var cards = document.getElementsByClassName('card');
+// for every card, add the dragDrop function
+for (var i = 0; i < cards.length; i++) {
+    dragDrop(cards[i]);
 }
 
-window.initMap = initMap;
+
+
+
+
+function dragDrop(element){
+    element.onmousedown = function(event) {
+        console.log(event.target.tagName)
+        if (event.target.tagName.toLowerCase() == 'a') {
+            console.log('NO DRAG')
+            return;
+        }
+        
+        //  start the process
+        // get ready to move: make an absolute and top z-index
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
+        // move it from any existing parents directly to the body
+        // to position it relative to the body
+        document.body.append(element);
+        // and put this absolutely positioned element under the pointer
+        moveAt(event.clientX, event.clientY);
+        // centers the element on the coordinates (pageX, pageY)
+        function moveAt(pageX, pageY) {
+          element.style.left = pageX - element.offsetWidth / 2 + 'px';
+          element.style.top = pageY - element.offsetHeight / 2 + 'px';
+        }
+        function onMouseMove(event) {
+          moveAt(event.clientX, event.clientY);
+        }
+        //  move the element on mousemove
+        document.addEventListener('mousemove', onMouseMove);
+        // drop the element, remove unneeded handlers
+        element.onmouseup = function() {
+          document.removeEventListener('mousemove', onMouseMove);
+          element.onmouseup = null;
+        };
+    };
+    
+}
+
 
