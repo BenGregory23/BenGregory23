074d3ffbd0d164e19d23c5c0a7f2298959256bb1
BenGregory23 <bengregorygbr@gmail.com>
2023-03-06 19:58:55 +0100

    Added file for Heroku

--- BenGregory/back/app.js
+++ BenGregory/back/app.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
