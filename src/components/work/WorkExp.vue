3bb9ea942bec2590bfc1c1fc7d756cef6af1df94
BenGregory23 <bengregorygbr@gmail.com>
2023-07-12 13:06:19 +0200

    now mobile friendly

--- src/components/work/WorkExp.vue
+++ src/components/work/WorkExp.vue
@@ -105,4 +105,74 @@ defineProps(
         font-weight: 300;
     }
 
+    @media (max-width: 768px) {
+        
+        .work-exp {
+            flex-direction: column;
+            justify-content: center;    
+            align-items: center;
+            height: fit-content;
+            min-height: 400px;
+            background-color: black;
+            margin: 20px;
+        }
+
+        .work-exp-image {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;    
+            align-items: center;
+            height: 250px;
+            background-size: cover;
+            width: 80%;
+            border-radius: 1px;
+            transition: all 0.2s ease-in-out;
+            margin: 10px;
+        }
+
+        .work-exp-image img {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+            border-radius: 1px;
+        }
+
+        .work-exp-text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;    
+            align-items: center;
+            height: 300px;
+            width: 80%;
+            border-radius: 1px;
+            transition: all 0.2s ease-in-out;
+
+            margin: 10px;
+            color : white;
+        }
+
+        .work-exp-text h3 {
+        
+            top:100%;
+            z-index: 1;
+            
+            background-color: white;
+            width: max-content;
+            color: black;
+            padding: 10px;
+        }
+
+        
+        .work-exp-text h4 {
+            margin: 0;
+            padding: 0;
+        }
+
+        .work-exp-text p {
+            margin: 0;
+            padding: 0;
+            font-weight: 300;
+        }
+    }
+
 </style>
