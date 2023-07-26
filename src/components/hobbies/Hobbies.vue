43ac8e15accbd1d03a10bf43e47c88a8928e637a
BenGregory23 <bengregorygbr@gmail.com>
2023-07-26 17:15:43 +0200

    changed hobby image

--- src/components/hobbies/Hobbies.vue
+++ src/components/hobbies/Hobbies.vue
@@ -11,7 +11,7 @@ import Hobby from './Hobby.vue'
 
         <div class="hobby-container">
             <Hobby title="Escalade" description="L'escalade de bloc est une discipline de l'escalade qui consiste à grimper sur des blocs de faible hauteur, sans utiliser de corde ni de baudrier. Je fait régulièrement du bloc depuis bientôt un an." image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.listeadrenaline.com%2Fwp-content%2Fuploads%2F2020%2F10%2FCafe-Bloc-Montreal.png&f=1&nofb=1&ipt=a7498610573b73abac0925fecab2a8e09d6752dbe8f9b718a164d51958a8fb6b&ipo=images"/>
-            <Hobby title="Jeux vidéo" description="Les jeux-vidéos font partis des choses qui me détendent et me permettent de garder contact avec des amis vivant dans d'autre ville ou pays." image="http://i.imgur.com/1aE1nMA.jpg"/>
+            <Hobby title="Jeux vidéo" description="Les jeux-vidéos font partis des choses qui me détendent et me permettent de garder contact avec des amis vivant dans d'autre ville ou pays." image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp9753860.jpg&f=1&nofb=1&ipt=7dfef7cc8e78c3dfc921a78b514c52de3c0c2b91ad166f213ce66bbed580aa3f&ipo=images"/>
             <Hobby title="Formule 1" description="La Formule 1, communément abrégée en F1, est une discipline de sport automobile. Lors du confinement je suis devenu fan de ce sport et je regarde quand je peux les qualifications et les courses." image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.programme.tv%2Fimgre%2Ffit%2F~2~program~e3346365f82e87aa.jpg%2F630x355%2Fquality%2F80%2Fformule-1-grand-prix-de-grande-bretagne.jpg&f=1&nofb=1&ipt=a69debb4bff7b0b006eb87cae6c710681932bb842a6b7b8677db0b09293c394d&ipo=images"/>
             
         </div>
